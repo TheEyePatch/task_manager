f573adbb9845f2aef09bd9b11e28a5805c47a09a
Leif Bersamina <leifbersamina@gmail.com>
2022-08-23 23:39:33 +0800

    create output basic index

--- app/controllers/outputs_controller.rb
+++ app/controllers/outputs_controller.rb
@@ -1,7 +1,18 @@
 class OutputsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :fetch_outputs, only: %i[index]
 
-  def index
-    @outputs = Output.all
+  def index; end
+
+  def edit; end
+
+  protected
+
+  def fetch_outputs
+    @outputs =
+    if employee_signed_in?
+      current_employee.outputs.includes(:regular_work, :overtime_work, :employee)
+    elsif reviewer_signed_in?
+      Output.includes(:regular_work, :overtime_work, :employee)
+    end
   end
 end
--- app/models/output.rb
+++ app/models/output.rb
@@ -1,6 +1,6 @@
 class Output < ApplicationRecord
   belongs_to :employee
   validates :employee_id, uniqueness: { scope: %i[date] }
-  has_many :overtime_works, class_name: 'Work::Overtime'
-  has_many :regular_works, class_name: 'Work::Regular'
+  has_one :overtime_work, class_name: 'Work::Overtime'
+  has_one :regular_work, class_name: 'Work::Regular'
 end
