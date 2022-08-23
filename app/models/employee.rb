74eeaa27b4c20a78fddb3980f37f45c7070c90bd
Leif Bersamina <leifbersamina@gmail.com>
2022-08-23 23:40:54 +0800

    set proper association for employee model

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,3 +1,5 @@
 class Employee < User
-  has_many :output
+  has_many :outputs
+  has_many :regular_works, through: :outputs, class_name: 'Work::Regular'
+  has_many :overtime_works, through: :outputs, class_name: 'Work::Overtime'
 end
