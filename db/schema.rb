c10adb9ef3b33bef1401dd6d887f82ebedb32e65
Leif Bersamina <leifbersamina@gmail.com>
2022-09-01 02:12:21 +0800

    add last_name for employee and reviewers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_29_095731) do
+ActiveRecord::Schema.define(version: 2022_08_31_174523) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 2022_08_29_095731) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "last_name"
     t.index ["account"], name: "index_users_on_account", unique: true
   end
 
